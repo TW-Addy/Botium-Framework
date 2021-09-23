bc4ab5394070f858caf70e875f977982b20632b2
Paul Phillips <paul.phillips@thoughtworks.com>
2021-09-23 16:55:48 -0500

    Read api url from env variable

--- resources/hooks.js
+++ resources/hooks.js
@@ -21,36 +21,4 @@ module.exports = class MyCustomLogicHook {
       setupApiMock(args);
       console.log(`MyCustomLogicHook onConvoBegin: ${convo.header.name} ${args}`)
     }
-  
-    onConvoEnd ({ convo, transcript, args }) {
-      console.log(`MyCustomLogicHook onConvoEnd ${convo.header.name}, conversation length: ${transcript.steps.length} steps`)
-    }
-  
-    onMeStart ({ convo, convoStep, args }) {
-      console.log(`MyCustomLogicHook onMeStart ${convo.header.name}/${convoStep.stepTag}, args: ${utils.inspect(args)}, meMessage: ${convoStep.messageText}`)
-    }
-  
-    onMeEnd ({ convo, convoStep, args }) {
-      console.log(`MyCustomLogicHook onMeEnd ${convo.header.name}/${convoStep.stepTag}, args: ${utils.inspect(args)}, meMessage: ${convoStep.messageText}`)
-    }
-  
-    onMe ({ convo, convoStep, args }) {
-      console.log(`MyCustomLogicHook onMe ${convo.header.name}/${convoStep.stepTag}, args: ${utils.inspect(args)}, meMessage: ${convoStep.messageText}`)
-    }
-  
-    onBotStart ({ convo, convoStep, args }) {
-      console.log(`MyCustomLogicHook onBotStart ${convo.header.name}/${convoStep.stepTag}, args: ${utils.inspect(args)}, expected: ${convoStep.messageText}`)
-    }
-  
-    onBotPrepare ({ convo, convoStep, args }) {
-      console.log(`MyCustomLogicHook onBotPrepare ${convo.header.name}/${convoStep.stepTag}, args: ${utils.inspect(args)}, expected: ${convoStep.messageText}`)
-    }
-  
-    onBotEnd ({ convo, convoStep, botMsg, args }) {
-      console.log(`MyCustomLogicHook onBotEnd ${convo.header.name}/${convoStep.stepTag}, args: ${utils.inspect(args)}, expected: ${convoStep.messageText}, meMessage: ${botMsg.messageText}`)
-    }
-  
-    onBot ({ convo, convoStep, botMsg, args }) {
-      console.log(`MyCustomLogicHook onBot ${convo.header.name}/${convoStep.stepTag}, args: ${utils.inspect(args)}, expected: ${convoStep.messageText}, meMessage: ${botMsg ? botMsg.messageText : null}`)
-    }
   }
--- test/botium.spec.js
+++ test/botium.spec.js
@@ -3,6 +3,8 @@ bb.helper.mocha().setupMochaTestSuite()
 
 const mockServer = require('../mockServer');
 
+process.env['API_URL'] = 'http://c195-2603-8080-f60c-43c9-381f-8e68-985f-ca6c.ngrok.io';
+
 before(() => {
   return mockServer.start();
 })
