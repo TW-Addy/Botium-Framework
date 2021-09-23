924df0d61eeef39af7c157fd267475a7359a693c
Paul Phillips <paul.phillips@thoughtworks.com>
2021-09-23 13:49:00 -0500

    Setup mock http server

--- test/botium.spec.js
+++ test/botium.spec.js
@@ -1,2 +1,16 @@
 const bb = require('botium-bindings')
 bb.helper.mocha().setupMochaTestSuite()
+
+const { mockServer } = require('http-mockserver');
+
+before(() => {
+  return mockServer.start(4001);
+})
+
+beforeEach(() => {
+  mockServer.clearAll();
+})
+
+after(() => {
+    return mockServer.stop();
+})
